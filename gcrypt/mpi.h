13e3796f3716a586901e72627f4683d18a81bf35
Yuri Dyachenko <yurial@gplvote.org>
2012-01-21 18:24:16 +0400

    add: base gcrypt::mpi

---
+++ gcrypt/mpi.h
@@ -0,0 +1,38 @@
+#ifndef GCRYPT_MPIH
+#define GCRYPT_MPIH
+
+#include <gcrypt.h>
+
+namespace gcrypt
+{
+
+template <bool secure>
+class mpi_template
+{
+protected:
+gcry_mpi_t  m_mpi;
+
+public:
+inline          mpi_template();
+inline          mpi_template(unsigned int nbits);
+inline          mpi_template(const mpi_template& origin);
+inline          ~mpi_template();
+
+mpi_template&   operator = (const mpi_template& rvalue);
+mpi_template&   operator = (const unsigned long rvalue);
+
+mpi_template&   swap(mpi_template& rvalue);
+
+                operator gcry_mpi_t& ();
+                operator const gcry_mpi_t& () const;
+};
+
+typedef mpi_template<false> mpi_t;
+typedef mpi_template<true>  smpi_t;
+
+#include "mpi.inc"
+
+} //namespace gcrypt
+
+#endif
+
---
+++ test/mpi.cpp
@@ -0,0 +1,11 @@
+#include "../gcrypt/mpi.h"
+
+int main(int argc, char* argv[])
+{
+using gcrypt::mpi_t;
+
+mpi_t a( 1024 );
+
+return 0;
+}
+
