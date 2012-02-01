78727f8871c1f49cdc4a7147c6a2ee3a7991c032
Yuri Dyachenko <yurial@gplvote.org>
2012-02-01 18:13:58 +0400

    add: gcrypt::error_t

---
+++ gcrypt/error.h
@@ -0,0 +1,15 @@
+#ifndef GCRYPT_ERRORH
+#define GCRYPT_ERRORH
+
+#include <gpg-error.h>
+#include <gcrypt.h>
+
+namespace gcrypt
+{
+
+typedef gcry_error_t error_t;
+
+} //namespace gcrypt
+
+#endif
+
