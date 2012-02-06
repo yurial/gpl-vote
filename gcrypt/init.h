d0713d473bdb7989b78b9bd5ca5747df4b5faa96
Yuri Dyachenko <yurial@gplvote.org>
2012-02-07 00:17:00 +0400

    fix: use throwif()

--- gcrypt/init.h
+++ gcrypt/init.h
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdexcept>
 #include <gcrypt.h>
+#include "exception.h"
 
 extern "C" 
 {
@@ -15,13 +16,9 @@ namespace gcrypt
 
 inline void init(int secmem) throw(std::runtime_error)
 {
-gcry_error_t err;
 /* Enabl pthread support */
-err = gcry_control( GCRYCTL_SET_THREAD_CBS, &gcry_threads_pthread );
-if ( 0 != err )
-    {
-    throw std::runtime_error( "libgcrypt set threads support" );
-    }
+error_t err = gcry_control( GCRYCTL_SET_THREAD_CBS, &gcry_threads_pthread );
+throwif( err );
 /* Version check should be the very first call because it
  * makes sure that important subsystems are intialized. */
 if ( !gcry_check_version( GCRYPT_VERSION ) )
@@ -31,17 +28,11 @@ if ( !gcry_check_version( GCRYPT_VERSION ) )
 /* Allocate a pool of 16k secure memory.  This make the secure memory
  *           available and also drops privileges where needed.  */
 err = gcry_control( GCRYCTL_INIT_SECMEM, secmem, 0 );
-if ( 0 != err )
-    {
-    throw std::runtime_error( "libgcrypt init secure memory" );
-    }
+throwif( err );
 /* ... If required, other initialization goes here.
  * Tell Libgcrypt that initialization has completed. */
 err = gcry_control( GCRYCTL_INITIALIZATION_FINISHED, 0 );
-if ( 0 != err )
-    {
-    throw std::runtime_error( "libgcrypt initialization" );
-    }
+throwif( err );
 }
 
 } //namespace gcrypt
